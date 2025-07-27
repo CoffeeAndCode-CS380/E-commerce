5565410ee46894433b625aededc7bea15cbe81a3
HirbodJb <hirbod.j@yahoo.com>
2025-07-27 16:07:39 -0700

    cart package added

--- clothing_website/src/main/java/module-info.java
+++ clothing_website/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ecommerce.com {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.sql;
 
     opens ecommerce.com to javafx.fxml;
     exports ecommerce.com;
